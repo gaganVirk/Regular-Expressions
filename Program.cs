a1acf29a6eea2618010ca42e5309aecb3f4bf135
gaganVirk <deep.virk@outlook.co.nz>
2019-07-11 14:11:21 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    public class Tester
+    {
+        public void Run()
+        {
+            string s1 = "One,Two,Three Liberty Associates, Inc.";
+            Regex theRegex = new Regex(" |, |,");
+            StringBuilder sBuilder = new StringBuilder();
+            int id = 1;
+
+            foreach(string subString in theRegex.Split(s1))
+            {
+                sBuilder.AppendFormat("{0}: {1}\n", id++, subString);
+            }
+            Console.WriteLine("{0}", sBuilder);
+        }
+    
+    static void Main(string[] args)
+        {
+            Tester t = new Tester();
+            t.Run();
+        }
+    }
+}
+
